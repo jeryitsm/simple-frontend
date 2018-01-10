82cffbdf4cd750cfc34a18fc5080a16695871b4a
Chaiyasit  Charoenphon <charoenphon.ch@outlook.com>
2018-01-11 01:04:07 +0700

    'editreactiveform2'

--- src/app/services/product-interceptor.ts
+++ src/app/services/product-interceptor.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ProductInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('Interceptor');
+        //clone and edit  (req.clone)
         return next.handle(req);
       }
 }
